1de94e5af11b68fca3a68ab6ffabccfc1e557d5c
Ambarish Joshi and Ehren Murdick <pair+ambarish+ehren@grindr.com>
2016-04-26 19:55:18 -0700

    updated the test for index view

--- spec/views/data_models/index.html.erb_spec.rb
+++ spec/views/data_models/index.html.erb_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe 'data_models/index.html.erb' do
-  let(:first_data_model)  { double(:data_model, name: 'Fraud detection') }
-  let(:second_data_model) { double(:data_model, name: 'Beer Daiper Detector') }
-  let(:third_data_model)  { double(:data_model, name: 'Food Recommender') }
+  let(:first_data_model)  { double(:data_model, name: 'Fraud detection', created_at: Time.now, updated_at: Time.now) }
+  let(:second_data_model) { double(:data_model, name: 'Beer Daiper Detector', created_at: Time.now, updated_at: Time.now) }
+  let(:third_data_model)  { double(:data_model, name: 'Food Recommender', created_at: Time.now, updated_at: Time.now) }
 
   let(:data_models)       { [ first_data_model, second_data_model, third_data_model ] }
 
@@ -13,14 +13,14 @@ describe 'data_models/index.html.erb' do
   end
 
   it 'has the name of the first model' do
-    expect(rendered).to(have_tag(:li, content: first_data_model.name))
+    expect(rendered).to(have_tag(:td, content: first_data_model.name))
   end
 
   it 'has the name of the second model' do
-    expect(rendered).to(have_tag(:li, content: second_data_model.name))
+    expect(rendered).to(have_tag(:td, content: second_data_model.name))
   end
 
   it 'has the name of the third model' do
-    expect(rendered).to(have_tag(:li, content: third_data_model.name))
+    expect(rendered).to(have_tag(:td, content: third_data_model.name))
   end
 end
